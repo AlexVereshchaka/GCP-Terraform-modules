952664770b2e28aa7759b6be37a18fff27cf57ba
Alex <86826701+AlexVereshchaka@users.noreply.github.com>
2022-05-06 20:34:36 +0300

    dfff

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "google" {
+   credentials = file("Creade_gcp.json")
+   project     = "dauntless-glow-338711"
+   region      = "us-weast1"
+   zone = "us-weast1-b"
+} 
+  
+resource "google_compute_instance" "my_server" {
+  name = "my-gcp-server"
+  machine_type = "f1-micro"
+boot_disk {
+  initialize_params {
+      image = "debian-cloud/debian-9"
+  }
+}
+natwork_interface {
+    network = "default"
+}
+}
+
