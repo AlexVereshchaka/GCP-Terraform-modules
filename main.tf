b6548e31608ca32f7d509e8301184cd55d5a9e38
Alex <86826701+AlexVereshchaka@users.noreply.github.com>
2022-05-06 21:19:31 +0300

    added changes

--- main.tf
+++ main.tf
@@ -1,23 +1,20 @@
 provider "google" {
-   credentials = file("gcp-creds.json")
-   project     = "dauntless-glow-338711"
-   region      = "us-weast1"
-   zone = "us-weast1-b"
-} 
-  
-resource "google_compute_instance" "my_server" {
-  name = "my-gcp-server"
+  credentials = file("gcp-creds.json") // export GCLOUD_KEYFILE_JSON="gcp-creds.json"
+  project     = "prod-251618"            // export GCLOUD_PROJECT="prod-251618"
+  region      = "us-west1"               // export GCLOUD_REGION="us-west1"
+  zone        = "us-west1-a"             // export GCLOUD_ZONE="us-west1-a"
+}
+
+resource "google_compute_instance" "my_web_server" {
+  name         = "my-gcp-server"
   machine_type = "f1-micro"
-boot_disk {
-  initialize_params {
-      image = "debian-cloud/debian-9"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9" // Image to use for VM
+    }
+  }
+  network_interface {
+    network = "default" // This Enable Private IP Address
+    access_config {}    // This Enable Public IP Address
   }
 }
-natwork_interface {
-    network = "default"
-}
-}
-
-
-
-
